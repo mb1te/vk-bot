036c7b6d40ce367e2e9baaa545e50e177d3702b9
mb1te <45900239+mb1te@users.noreply.github.com>
2020-03-01 12:27:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token = '636b5ca075a225c1ae9ae8f36071ea8b1e3a562aab156c4c2095ead56ea3a16d251ccd708f2197eccd755'
+token = 'TOKEN'
 
 def write_msg(user_id, message):
     vk.method('messages.send', {'user_id' : user_id, 'message' : message})
